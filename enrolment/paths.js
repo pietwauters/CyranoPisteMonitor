c9700e3545efab2a26856279a07173b090b678b2
pietwauters <67114377+pietwauters@users.noreply.github.com>
2026-03-10 16:14:34 +0100

    Revert "Improved enrolment"
    This reverts commit 420b0c1dca0c575c258f1fd79f8a132c894ddf4d.

--- enrolment/paths.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-const ENROLMENT_CA = '/home/atlas/scoring-broker/ca';
-const ENROLMENT_DEVICES = '/home/atlas/scoring-broker/devices';
-const PAIRING_FILE = '/var/lib/scoring-broker/pairing.json';
-
-module.exports = { ENROLMENT_CA, ENROLMENT_DEVICES, PAIRING_FILE };
